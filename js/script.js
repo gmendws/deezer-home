79731f90ef377a5f4d95ce7945e5a3b473ffc1d4
Guilherme Mendes <gui.mendes_69@hotmail.com>
2022-10-30 14:54:30 -0300

    alteracoes js
    -> Ajustado chamada da api de login, anteriormente feita pelo axios agora por ajax e json.
    -> Criada funções para chamada da api de lyrics, trazendo o nome das musicas após utilizar o box para pesquisar.
    ->

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
   function logado() {
-    document.querySelector('#loggedin').style.visibility = 'hidden';
+    if(getToken()){
+      document.querySelector('#loggedout').style.visibility = 'hidden';  
+      document.querySelector('#loggedin').style.visibility = 'visible';    
+    } 
+    else {
+      document.querySelector('#loggedin').style.visibility = 'hidden'; 
+      document.querySelector('#loggedout').style.visibility = 'visible'; 
+    }
+    
   }
-  
-  function login() {
-    var valida = false;
+
+  function RequestLogin(){
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
     if (document.getElementById('email').value.length < 3) {
       document.getElementById('validacao').innerHTML = 'Email invalido';
     }
@@ -11,34 +21,118 @@
       document.getElementById('validacao').innerHTML = 'Senha invalida';
     }
     else{
-      valida = true;
+      login(email, password, function(response){
+        if(response.error){
+          document.getElementById('validacao').innerHTML = 'Login incorreto';query
+        }
+        else{
+          localStorage.setItem("loginToken", response.token);
+          logado();
+        }
+      })
     }
-    if (valida === true){
-      const params = {
-        username: document.getElementById('email').value,
-        password: document.getElementById('password').value
-      };
+  }
+
+  function RequestSearch(){
+    var music = document.getElementById('search-music-value').value;
+
+    searchMusic(music, 10, function(res){
+      Results(res);
+    });
+  }
+
+  function searchMusic(query, limit, callback){
+    let request = new XMLHttpRequest();
+
+    request.open("GET", "https://api.lyrics.ovh/suggest/" + query, true);
+    request.setRequestHeader("Content-Type", "application/json; charset=utf-8");
   
-      axios.get('https://reqres.in/api/users', params)
-        .then(res => {
-          console.log(res);
-          const { token } = res.data;
-          localStorage.setItem('token', token);
-          document.getElementById('validacao').innerHTML = 'Login efetuado';
-          document.querySelector('#loggedout').style.visibility = 'hidden';
-          document.querySelector('#loggedin').style.visibility = 'visible';
-        })
-        .catch(error => {
-          document.getElementById('validacao').innerHTML = 'Usuário não encontrado';
-        });
+    request.onreadystatechange = function () {
+  
+      if(request.readyState !== 4){
+        return;
+      }
+  
+      let content = [];
+  
+      if(request.status === 200){
+  
+        let data = JSON.parse(request.responseText).data;
+  
+        limit = (limit >= 15 ? 15 : limit);
+        let max = (limit > data.length ? data.length : limit);
+  
+        for(let i = 0; i < max ; i++ ) {
+  
+          let dt = data[i];
+  
+          content[i] = {
+  
+            artist: {
+              name: dt.artist.name,
+              picture: dt.artist.picture
+            },
+  
+            album: {
+              title: dt.album.title,
+              cover: dt.album.cover
+            },
+  
+            title: dt.title
+          };
+        }
+      }
+  
+      callback(content);
+    };
+  
+    request.send();
+  }
+
+  function Results(res){
+    let searchBox = document.querySelector(".search-music");
+    let list = document.querySelector(".music-box");
+  
+    for(let i = 0; i < res.length; i++){
+      let div = document.createElement("div");
+      let p = document.createElement("p");
+      
+      p.classList.add("music-name");
+  
+      p.innerHTML = res[i].title;
+  
+      div.appendChild(p);
+      list.appendChild(div);
     }
+  
+    searchBox.appendChild(list);
+  }
+  
+  function login(email, password, callback) {
+    let request = new XMLHttpRequest();
+    request.open("POST", "https://reqres.in/api/login", true);
+    request.setRequestHeader("Content-Type", "application/json; charset=utf-8")
+    request.onreadystatechange = function () {
+  
+      if(request.readyState !== 4){
+        return;
+      }
+  
+      callback(JSON.parse(request.responseText));  
+    };
+  
+    request.send(JSON.stringify({
+      email: email,
+      password: password
+    }));
   }
   
   function getToken() {
-    return localStorage.getItem('token');
+    return localStorage.getItem('loginToken');
   }
   
   function logout() {
     localStorage.clear();
     location.reload();
   }
+
